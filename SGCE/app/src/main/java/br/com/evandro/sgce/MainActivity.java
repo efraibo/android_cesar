ab24e0e89aec17d07d9aaea5f14f8f8707a564da
efraibo <efraibo@gmail.com>
2015-11-30 23:07:21 -0300

    Primeiro Commit

---
+++ SGCE/app/src/main/java/br/com/evandro/sgce/MainActivity.java
@@ -0,0 +1,21 @@
+package br.com.evandro.sgce;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
